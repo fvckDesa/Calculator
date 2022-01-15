18d4f8958f41f41413e6f8a046f6ab9c8886881d
Luca De Santis <lucaroma2705@gmail.com>
2022-01-15 22:27:18 +0100

    completate tutte le funzionalità della calcolatrice

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # calculator
 
 comando push ramo: git push origin calculator-v2.0
+
+//TODO creare lista a scomparsa con tutti i calcoli e i risultati eseguiti
--- script.js
+++ script.js
@@ -1,94 +1,99 @@
-const btnsNumber = document.querySelectorAll('.number');
+const btnsNumber = document.querySelectorAll(".number");
 
-const btnsOperation = document.querySelectorAll('.operationSimbol');
+const btnsOperation = document.querySelectorAll(".operationSimbol");
 
 const ugu = document.querySelector("#ugu");
 const canc = document.querySelector("#can");
 const back = document.querySelector("#back");
 
+const point = document.querySelector("#point");
+
 const displayOperation = document.getElementById("operation");
 const displayNum = document.getElementById("num");
 
-let operationString = '';
+let operationString = "";
 
-btnsNumber.forEach(btn => {
-    btn.param = btn.textContent;
-    btn.addEventListener('click', print);
+btnsNumber.forEach((btn) => {
+  btn.param = btn.textContent;
+  btn.addEventListener("click", print);
 });
 
-btnsOperation.forEach(btn => {
-    btn.param = btn.textContent;
-    btn.addEventListener('click', print);
+btnsOperation.forEach((btn) => {
+  btn.param = btn.textContent;
+  btn.addEventListener("click", print);
+  btn.addEventListener('click', () => {
+    if (!!operationString.match(/[^0-9 .]/g)) {
+        btnsOperation.forEach((btn) => {
+          btn.removeEventListener("click", print);
+        });
+      }
+  });
 });
 
 function print(e) {
-    if(checkThereIsOperator(operationString)){
-        btnsOperation.forEach(btn => {
-            btn.removeEventListener('click', print);
-        });
-    }
-    operationString += e.target.param;
-    displayNum.innerText = operationString;
+  operationString += e.target.param;
+  displayNum.innerText = operationString;
 }
 
-function checkThereIsOperator(string) {
-    return /[^0-9]/g.test(string);
-}
-
-ugu.addEventListener('click', resolve);
+ugu.addEventListener("click", resolve);
 
 function resolve() {
-    let num1, num2, resulte, simbol;
+  let num1, num2, resulte, simbol;
 
-    displayOperation.innerText = operationString;
+  displayOperation.innerText = operationString;
 
-    simbol = operationString[operationString.search(/[^0-9]/g)];
+  simbol = operationString[operationString.search(/[^0-9 .]/g)];
 
-    let tempArr = operationString.split(/[^0-9]/g);
+  let tempArr = operationString.split(/[^0-9 .]/g);
 
-    num1 = +tempArr[0];
-    num2 = +tempArr[1];
+  num1 = +tempArr[0];
+  num2 = +tempArr[1];
 
-    switch (simbol) {
-        case '+':
-            resulte = (num1 + num2).toString();
-            break;
-        case '-':
-            resulte = (num1 - num2).toString();
-        case 'x':
-            resulte = (num1 * num2).toString();
-            break;
-        case '÷':
-            resulte = (num1 / num2).toString();
-            break;
-        default:
-            resulte = operationString;
-    }
-    clear(resulte);
+  switch (simbol) {
+    case "+":
+      resulte = (num1 + num2).toString();
+      break;
+    case "-":
+      resulte = (num1 - num2).toString();
+      break;
+    case "x":
+      resulte = (num1 * num2).toString();
+      break;
+    case "÷":
+      resulte = (num1 / num2).toString();
+      break;
+    default:
+      resulte = operationString;
+  }
+  clear(resulte);
 }
 
-canc.addEventListener('click', clear);
+canc.addEventListener("click", clear);
 
-function clear(string = '0') {
-    operationString = '';
-    if(typeof string === 'object'){
-        displayNum.innerText = '0';
-    }else {
-        operationString = string;
-        displayNum.innerText = operationString;
-    }
-    btnsOperation.forEach(btn => {
-        btn.addEventListener('click', print);
-    });
+function clear(string = "0") {
+  operationString = "";
+  if (typeof string === "object") {
+    displayNum.innerText = "0";
+  } else {
+    operationString = string;
+    displayNum.innerText = operationString;
+  }
+  btnsOperation.forEach((btn) => {
+    btn.addEventListener("click", print);
+  });
 }
 
-back.addEventListener('click', () => {
-    let shift = operationString.split('');
-    shift.pop();
-    operationString = shift.join('');
-    displayNum.innerText = operationString === '' ? '0' : operationString;
+back.addEventListener("click", () => {
+  let shift = operationString.split("");
+  shift.pop();
+  operationString = shift.join("");
+  displayNum.innerText = operationString === "" ? "0" : operationString;
 });
 
-
-
-
+point.addEventListener("click", () => {
+  let tempArr = operationString.split(/[^0-9 .]/g);;
+  if (!tempArr[0].includes(".") || !tempArr[1].includes(".")) {
+    operationString === "" ? (operationString += "0.") : (operationString += ".");
+    displayNum.innerText = operationString;
+  }
+});
